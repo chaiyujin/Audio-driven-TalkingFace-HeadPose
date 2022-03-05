84f1819ca23697cc42dee16d742669e33dd59f35
yuki-chai <chaiyujin@gmail.com>
2022-03-05 19:41:58 +0800

    watson

--- yk_run.sh
+++ yk_run.sh
@@ -1,9 +1,19 @@
+source nohup_run.sh
 source yk_functions.sh
 
-RUN_YK_EXP \
-  --data_src=celebtalk --speaker=m001_trump --use_seqs="trn-000,trn-001,vld-000,vld-001" \
-  --epoch_a2e=50  \
-  --test  \
-  --media_list=../../media_list.txt \
-  --dump_meshes \
-;
+# RUN_YK_EXP \
+#   --data_src=celebtalk --speaker=m001_trump --use_seqs="trn-000,trn-001,vld-000,vld-001" \
+#   --epoch_a2e=50  \
+#   --test  \
+#   --media_list=../../media_list.txt \
+#   --dump_meshes \
+# ;
+
+# NOHUP_RUN --device=0 --include=yk_functions.sh -- \
+# RUN_YK_EXP \
+#   --data_src=celebtalk --speaker=f000_watson --use_seqs="trn-000,trn-001,vld-000,vld-001" \
+#   --epoch_a2e=50  \
+#   --test  \
+#   --media_list=../../media_list.txt \
+#   --dump_meshes \
+# ;
