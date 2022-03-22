75d856375413161a00e09106c31b101249524f9d
yuki-chai <chaiyujin@gmail.com>
2022-03-22 18:16:42 +0800

    Update run.sh

--- yk_scripts/run.sh
+++ yk_scripts/run.sh
@@ -1,17 +1,26 @@
 source yk_scripts/nohup_run.sh
 
-# ? Suggest training args: --epoch_a2e=50
-# ? Suggest testing  args: --epoch_a2e=50 --test --media_list=../../media_list.txt
+# * ---------------------------------------------------------------------------------------------------------------- * #
+# *              Our data: VOCASET pre-train general model -> finetune with data tracked by our tracker              * #
+# * ---------------------------------------------------------------------------------------------------------------- * #
 
-# * Our data: VOCASET pre-train general model -> finetune with data tracked by our tracker
+# ? Suggest training args: --epoch_a2e=40
+# ? Suggest testing  args: --epoch_a2e=20 --test --media_list=../../media_list.txt
+
 source yk_scripts/flame/functions.sh
 # NOHUP_RUN --device=0 --include=yk_scripts/flame/functions.sh -- \
 # > Keep avoffset
 # RUN_YK_EXP --data_src=celebtalk --speaker=m001_trump "$@";
 # > Manually correct avoffset
-# RUN_YK_EXP --data_src=celebtalk --speaker=m001_trump --avoffset_ms=33.333333333333336 "$@";
+# RUN_YK_EXP --data_src=celebtalk --speaker=m001_trump   --avoffset_ms=33.333333333333336 "$@";
+# RUN_YK_EXP --data_src=celebtalk --speaker=m000_obama   --avoffset_ms=100.0              "$@";
+# RUN_YK_EXP --data_src=celebtalk --speaker=f000_watson  --avoffset_ms=133.33333333333334 "$@";
+# RUN_YK_EXP --data_src=celebtalk --speaker=f001_clinton --avoffset_ms=100.0              "$@";
 
-# * Their pre-trained general model -> finetune with data tracked by their tracker
+# * ---------------------------------------------------------------------------------------------------------------- * #
+# *                  Their pre-trained general model -> finetune with data tracked by their tracker                  * #
+# * ---------------------------------------------------------------------------------------------------------------- * #
+
 source yk_scripts/bfm/functions.sh
 # NOHUP_RUN --device=0 --include=yk_scripts/bfm/functions.sh -- \
 # RUN_YK_EXP --data_src=celebtalk --speaker=m001_trump "$@";
